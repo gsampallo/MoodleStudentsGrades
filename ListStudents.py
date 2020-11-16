8a9f8e7915afa805ec546d8eff97b3720f8f620d
Guillermo Sampallo <guille@gmail.com>
2020-11-16 19:22:46 -0300

    Falta la lista que tenga el correo

--- ListStudents.py
+++ ListStudents.py
@@ -29,13 +29,30 @@ class ListStudents:
                  else:
                     print(f'\t{row[1]},{row[0]} {row[2]}')
                     data = []
-                    data.append(row[1]+","+row[0])
+                    
+                    query = self.get_degree(row[1]+", "+row[0])
+                    if(query[0] == 'Not found'):
+                        data.append('Not found')
+                        data.append(row[1]+", "+row[0])
+                        data.append('Not found')
+                    else:
+                        data.extend(self.get_degree(row[1]+", "+row[0]))
+                    #data.append(row[1]+","+row[0])
                     data.append(row[2])
 
-                    col_grade = 2
+                    
+
+                    col_grade = 3
+                    
+                    for col_name in self.parameters["teoria"]:
+                        grades = self.get_grades(row[2],col_name["examen"])
+                        data.append(grades)
+                        col_grade += 1
                     
                     evpAprobada = 0
                     integrador = 0
+
+
 
                     for col_name in self.parameters["columnas"]:
 
@@ -51,12 +68,13 @@ class ListStudents:
                             evpAprobada += 1
                         if(col_name["integradora"] == 1 and grades >= 6):
                             integrador += 1
-                        
+
+                    
                     if(evpAprobada >= 4 and integrador == 1):
-                        self.ws.write(line_count,22,"REGULAR")
+                        self.ws.write(line_count,len(data),"REGULAR")
 
                     else:
-                        self.ws.write(line_count,22,"LIBRE")
+                        self.ws.write(line_count,len(data),"LIBRE")
 
 
                     self.add_student(line_count,data)
@@ -81,17 +99,49 @@ class ListStudents:
             except ValueError as err:
                 grades = 0
             
-        else:
-            print(grades)
+        #else:
+        #    #print(grades)
+
         return grades
+
+
+    def get_degree(self,name):
+        row = [] 
+        notFound = True
+        file = open("listado.csv", mode='r',encoding='UTF-8')
+        for data in file:
+            fields = data.split(";")
+
+            print(fields[2])
+            if(name.upper() == fields[2].upper()):
+                print(data)
+                row.append(fields[1])
+                row.append(fields[2])
+                row.append(fields[3])
+                notFound = False
+                break
+            
+        if(notFound):
+            row.append('Not found')
+            row.append('Not found')
+            row.append('Not found')
+            
+        
+        return row
+            
 
     def create_xls(self):
         output_xls = self.parameters["output"]+"/"+datetime.now().strftime("%Y%m%d-%H%M%S")+".xls"
         self.wb = xlwt.Workbook()
         self.ws = self.wb.add_sheet("Notas",cell_overwrite_ok=True)
-        self.ws.write(0, 0, "Apellido(s),Nombre")
-        self.ws.write(0, 1, "Dirección Email")
-        col_num = 2
+        self.ws.write(0, 0, "Legajo")
+        self.ws.write(0, 1, "Apellido(s),Nombre")
+        self.ws.write(0, 2, "Carrera")
+        self.ws.write(0, 3, "Dirección Email")        
+        col_num = 4
+        for col_name in self.parameters["teoria"]:
+            self.ws.write(0, col_num, col_name["name"])
+            col_num += 1            
         for col_name in self.__parameters["columnas"]:
             self.ws.write(0, col_num, col_name["name"])
             col_num += 1
